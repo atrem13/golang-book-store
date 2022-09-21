e1dba441e04951a8563b7a684003282aee2340a2
atrem13 <mertayoga17.my@gmail.com>
2022-09-21 14:02:44 +0800

    update

--- controllers/books.go
+++ controllers/books.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"book-store/models"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
@@ -11,4 +12,19 @@ func FindBooks(c *gin.Context) {
 	models.DB.Find(&books)
 
 	c.JSON(200, gin.H{"data": books})
+}
+
+func CreateBook(c *gin.Context) {
+	var input models.CreateBookInput
+
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error: ": err.Error()})
+		return
+	}
+
+	book := models.Book{Title: input.Title, Author: input.Author}
+	models.DB.Create(&book)
+
+	c.JSON(http.StatusOK, gin.H{"data": book})
+
 }
